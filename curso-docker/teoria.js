24ec185b204122f506f591dfa118740d200cd984
Juan Carlos Serrudo <49172116+serrudojc@users.noreply.github.com>
2020-03-29 21:48:40 -0300

    teoria

--- curso-docker/teoria.js
+++ curso-docker/teoria.js
@@ -47,8 +47,110 @@ la imagen y ejecutarla en nuestro host.
 //***************************************************
 
 Instalacion
-Se ejecuta solo desde linux.
 
 
+//***************************************************
+HOla mundo. 
+Descargamos la imagen hello del repositorio, el cual tiene un script que tiene hola mundo
+
+1- Descargamos una imagen de un repositorio
+docker image
+docker image pull fernando93d/hello
+
+2- docker image ls
+
+3- docker container
+
+docker container run --help
+
+docker container run fernando93d/hello
+En el contenedor solo tengo un script en python que dibuja el mensaje.
+
+
+
+
+//********************************************************
+//Comando basicos
+docker container //para ver todos los comandos
+docker container ls --help //ayuda sobre el comando
+
+docker container ls -a  //muestra todos los contenedores corriendo y apagados.
+Vemos la estructura, el id del contendor,IMAGE que indica la imagen q el contenedor usa, COMMAND, CREATED, 
+STATUS (si está activo o detenido), PORTS y NAMES
+
+docker container run nombreImagen   //ejecuta un contenedor
+
+docker container rm //elimina un contendor seguido del ID o del NOMBRE
+docker container rm idContenedor/NAME
+
+
+docker container create fernando93d/hello   //Con ls -a veo el STATUS Created del contenedor.
+Luego de generar el contenedor, vamos a iniciarlo
+docker container start ID/NAME              //Con ls -a en STATUS vemos que ha sido detenido
+
+
+
+
+//---------------------------------------------------------
+//Modo interactivo en Docker
+Descargamos la imagen de Ubuntu
+docker image pull ubuntu    //por default descarga la ultima version
+hay tags en el docker hub, especificar las diferentes versiones de ubuntu https://hub.docker.com/_/ubuntu
+
+
+inspeccionamos las imagenes 
+docker image ls
+
+corremos el contenedor
+docker container run ubuntu:lastest
+
+inspeccionamos los contenedores
+docker container ls
+
+docker container ls -a
+vemos que  nuestro contenedor está apagado, esto es pq dentro del contenedor NO ESTAMOS EJECUTANDO 
+NINGUN PROCESO.
+
+descargamos la imager de nginx, la buscamos en el docker hub
+docker image pull nginx
+docker container run nginx  //no pasa nada
+procedemos abrir otra terminal y ejecutamos
+docker container ls
+vemos q  vemos STATUS y el contenedor activo, el contenedor no se apaga automaticamente. Ctrl+c
+
+Modo interactivo
+es una forma de ejecutar una terminal dentro del contenedor
+usamos flag -i mantiene a la entrada standar abierta ,-t genera una nueva terminal. Generamos un proceso dentro del contendor + terminal
+
+docker container run -it ubuntu
+
+Ejecutamos y vemos q ahora estamos dentro de la instancia del contenedor.
+Vamos a otra terminal y ejecutamos docker container ls
+Pero si hacemos exit dentro del proceso, el contendor se apaga
+Para evitar esto, usamos flag -d q ejecuta al contendor en segundo plano.
+
+docker container run -itd ubuntu
+
+
+
+
+//**************************************************************
+// Ejecutar comandos dentro de un contenedor
+docker container attach IDContenedor //agreganos a la salida, entrada y error standard corriendo dentro del contenedor
+Nos agrega a la tarminal
+Pero cuando hacemos exit, terminamos el proceso
+
+Ejecutamos de nuevo el contenedor
+docker container exec --help
+docker container exec IDContenedor ls 
+
+Al ejecutar eso, ejecutamos comandos.
+
+docker container exec -it IDcontenedor bash //ahora ejecutamos y salimos con exit, sigue el proceso del contenedor
+
+
+
+
+//*******************************************************************
 
 */
